4a16f58e24c5b485eb2a1786f750c4f795882bed
Студент1 <1@DESKTOP-7RO4MU5>
2022-12-16 11:25:02 +0300

    Добавьте файлы проекта.

---
+++ Bit_Counting/CountingBit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit_Counting;
+
+public class CountingBit
+{
+    public static int CountBits(int n)
+    {
+        int counter = 0;
+        string BinaryCode = Convert.ToString(n, 2);
+        for (int i = 0; i < BinaryCode.Length; i++)
+        {
+            if (BinaryCode[i] == '1')
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
---
+++ Bit_Counting/Program.cs
@@ -0,0 +1,4 @@
+﻿using Bit_Counting;
+
+var result = CountingBit.CountBits(1234);
+Console.WriteLine($"Количество едениц в двоичном коде - {result}");
